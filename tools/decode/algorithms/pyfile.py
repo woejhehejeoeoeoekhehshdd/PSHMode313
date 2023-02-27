fe52b4d475067faa4ede9b28424e65d4ed0ba60c
woejhehejeoeoeoekhehshdd <77845409+woejhehejeoeoeoekhehshdd@users.noreply.github.com>
2023-02-27 23:36:43 +0300

    Add files via upload

---
+++ tools/decode/algorithms/pyfile.py
@@ -0,0 +1,52 @@
+import os
+import sys
+sys.path.insert(0, os.environ.get("VIRTUAL_ENV") + "/tools/decode")
+
+from types import CodeType
+from typing import Union
+from zipfile import ZipFile
+from utils.bytecode import clean_source
+
+def open_file(filename) -> Union[str, bytes, CodeType]:
+    try:
+        with open(filename, "r") as r_file:
+            return clean_source(r_file.read())
+    except UnicodeDecodeError:
+        with open(filename, "rb") as rb_file:
+            return rb_file.read()
+
+
+def open_python_file(filename) -> Union[str, bytes, CodeType]:
+    source = open_file(filename)
+    if get_source_type(source) == "zip":
+        archive = ZipFile(filename)
+        py_filename = archive.filelist[0].filename
+        source = archive.read(py_filename)
+        if get_source_type(source) == "py":
+            return clean_source(source)
+        return source
+    return source
+
+
+def get_source_type(source) -> str:
+    try:
+        compile(source, "<string>", "exec")
+        return "py"
+    except Exception:
+        if type(source) == str:
+            source = source.encode("utf-8")
+        if b'PK\x03\x04' in source:
+            return "zip"
+        else:
+            try:
+                source.decode()
+                return "py"
+            except Exception:
+                return "pyc"
+
+
+def get_file_type(filename) -> str:
+    source = open_file(filename)
+    return get_source_type(source)
+
+
